f98bffa8303d70202140afa8cef0228258e1e328
Lavanya Prakash jampana <vamsiwillb@gmail.com>
2023-12-02 23:03:32 +0530

    search Implementation

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,54 +1,116 @@
-// src/DataTable.js
-import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import React, { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { TextField, InputAdornment, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import DeleteIcon from '@mui/icons-material/Delete'; // Correct import statement
 
+// Define columns for the data grid
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-];
-
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'email', headerName: 'Email', width: 130 },
+  { field: 'role', headerName: 'Role', type: 'number', width: 90 },
 ];
 
 export default function DataTable() {
+  // State to hold the rows and filtered rows
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  
+  // State for search text, pageSize, and current page
+  const [searchText, setSearchText] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [pageSize, setPageSize] = useState(10); // Number of rows per page
+  const [page, setPage] = useState(1);
+
+  // State to manage selected rows
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  // Fetch data on component mount
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setRows(data);
+        setFilteredRows(data);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Update filtered rows when search text changes
+  useEffect(() => {
+    const filtered = rows.filter((row) =>
+      // Check if any property of the row includes the search text
+      Object.values(row).some((value) => String(value).toLowerCase().includes(searchText.toLowerCase()))
+    );
+    setFilteredRows(filtered);
+    setPage(1); // Reset page when search is performed
+  }, [searchText, rows]);
+
+  // Calculate total pages for pagination
+  const totalPages = Math.ceil(filteredRows.length / pageSize);
+
+  // Function to handle row selection
+  const handleRowSelection = (selected) => {
+    setSelectedRows(selected.selectionModel);
+  };
+
+ 
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
+    <div style={{ height: 500, width: '100%' }}>
+      {/* Search Bar */}
+      <TextField
+        variant="outlined"
+        label="Search"
+        placeholder="Search..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {/* Search Icon */}
+              <IconButton className="search-icon">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
         }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
       />
+
+      {/* Data Grid with row selection */}
+      <DataGrid
+        rows={filteredRows.slice((page - 1) * pageSize, page * pageSize)}
+        columns={columns}
+        pageSize={pageSize}
+        pagination
+        page={page}
+        onPageChange={(newPage) => setPage(newPage)}
+        rowCount={filteredRows.length}
+        rowsPerPageOptions={[pageSize]}
+        paginationMode="server"
+        autoHeight
+        checkboxSelection
+        onSelectionModelChange={handleRowSelection}
+      />
+
+    
+
+      {/* Pagination Controls */}
+      <div>
+        <button onClick={() => setPage(1)}>First Page</button>
+        <button onClick={() => setPage(Math.max(page - 1, 1))}>Previous Page</button>
+        <button onClick={() => setPage(Math.min(page + 1, totalPages))}>Next Page</button>
+        <button onClick={() => setPage(totalPages)}>Last Page</button>
+        <span>{`Page ${page} of ${totalPages}`}</span>
+      </div>
     </div>
   );
 }
